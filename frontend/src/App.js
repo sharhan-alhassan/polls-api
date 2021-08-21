4f12ae13b00e155fb409395a7ca554cc44180f32
sharhan-alhassan <sharhanalhassan@gmail.com>
2021-08-21 10:03:43 +0000

    modified README.md

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+
+const list  = [
+    {
+        "id": 1,
+        "title": "laravel development",
+        "body": "If you make changes to your local copy and push them to GitHub and need to push them to production, here’s a quick how-to (logged in as our user from earlier)."
+    },
+    {
+        "id": 2,
+        "title": "django tutorials",
+        "body": "If you make changes to your local copy and push them to GitHub and need to push them to production, here’s a quick how-to (logged in as our user from earlier)."
+    },
+    {
+        "id": 3,
+        "title": "Sahih Bukhari",
+        "body": "If you make changes to your local copy and push them to GitHub and need to push them to production, here’s a quick how-to (logged in as our user from earlier)."
+    }
+]
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { list };
+  }
+
+  render() {
+    return (
+       <div>
+         {this.state.list.map(item => (
+           <div key={item.id}>
+             <h1>{item.title}</h1>
+             <p>{item.body}</p>
+           </div>
+         ))}
+       </div>
+    );
+  }
 }
 
 export default App;
